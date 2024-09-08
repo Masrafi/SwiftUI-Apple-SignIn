56b9730ff02264a71c0dc29ae75d6493017b5707
Masrafi <masrafianam@gmail.com>
2024-09-08 18:54:47 +0600

    Initial Commit

---
+++ Apple Sign In/Apple_Sign_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Sign_InApp.swift
+//  Apple Sign In
+//
+//  Created by Md Khorshed Alam on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Sign_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
